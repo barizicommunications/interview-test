d84f7f44749e8192cba32ada2bcff81a41f0b691
patrickmwangi017 <mwangipatrick017@gmail.com>
2023-02-16 15:28:06 +0300

    add html scaffolding to layout file

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+
+        <div class="header">
+            <h2>Blog Name</h2>
+        </div>
+
+        <div class="row">
+            <!-- BLOG BODY -->
+        </div>
+
+        <div class="footer">
+            <h2>Footer</h2>
+        </div>
+
+    </body>
+</html>
