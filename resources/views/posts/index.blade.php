1659af8c311a925fc0b147407fe89f2d8c12b003
patrickmwangi017 <mwangipatrick017@gmail.com>
2023-02-16 20:46:58 +0300

    add loop for posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-            <img src="{{$post->featured_image_url}}" alt="{{$post->title}}">
-            <h2>{{$post->title}}</h2>
-            <p><small>{{$post->slug}}</small></p>
-            <p>{{$post->body}}</p>
+    @foreach($posts as $post)
+        <div class="row">
+            <div class="col-md-12">
+                <img src="{{$post->featured_image_url}}" alt="{{$post->title}}">
+                <h2>{{$post->title}}</h2>
+                <p><small>{{$post->slug}}</small></p>
+                <p>{{$post->body}}</p>
+            </div>
         </div>
-    </div>
+    @endforeach
 @endsection
