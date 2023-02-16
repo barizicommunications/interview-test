aab3c766ef9780a68bbfc9b2d594060f88a3acd8
patrickmwangi017 <mwangipatrick017@gmail.com>
2023-02-16 20:31:49 +0300

    add relationships for all models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,4 +19,12 @@ class Post extends Model
             ]
         ];
     }
+
+    public function author() {
+        return $this->belongsTo(Author::class, 'author_id');
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 }
