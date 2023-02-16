7e25071fe1262245907e33e54d013c9b607ca13f
patrickmwangi017 <mwangipatrick017@gmail.com>
2023-02-16 20:17:57 +0300

    add sluggable to posts model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
+
+    use Sluggable;
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
