ffcb4372d99f41b3a006e4b0314123e224019e2b
patrickmwangi017 <mwangipatrick017@gmail.com>
2023-02-16 20:19:40 +0300

    add seeder content for posts

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Post;
 
 class PostSeeder extends Seeder
 {
@@ -14,5 +15,6 @@ class PostSeeder extends Seeder
     public function run()
     {
         //
+        factory(Post::class, 15)->create();
     }
 }
